86b48697ea52c92144ce52ceae050b5ac7001f4d
chiyeon01 <cyj588369@gmail.com>
2025-03-05 03:47:00 +0000

    내리막 길

--- 동적계획법(Dynamic Programming)/BOJ_1520(G3).py
+++ 동적계획법(Dynamic Programming)/BOJ_1520(G3).py
@@ -1,41 +1,38 @@
 import sys
+import heapq
 
 input = sys.stdin.readline
 
-M, N = map(int, input().split())
+m, n = map(int, input().split())
 board = []
-dp = [[0 for _ in range(N)] for _ in range(M)]
-dp[0][0] = 1
+dp = [[0 for _ in range(n)] for _ in range(m)]
+dp[0][0] = 1 # dp[r][c] : r,c까지 가는 경우의 수
+visited = [[False for _ in range(n)] for _ in range(m)]
+visited[0][0] = True
 
-for _ in range(M):
+for i in range(m):
     board.append(list(map(int, input().split())))
 
-dr = [-1, 0, 1, 0]
-dc = [0, 1, 0, -1]
-for r in range(M):
-    for c in range(N):
-        current = board[r][c]
-        for k in range(4):
-            nr = r + dr[k]
-            nc = c + dc[k]
-            if nr < 0 or nr >= M or nc < 0 or nc >= N:
-                continue
-            if board[nr][nc] < current:
-                dp[nr][nc] += dp[r][c]
-                
-for r in range(M):
-    for c in range(N):
-        current = board[r][c]
-        s = 0
-        if r == c == 0:
+queue = []
+heapq.heappush(queue, [-1 * board[0][0], 0, 0]) # value, x, y
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+while queue:
+    value, x, y = heapq.heappop(queue)
+    value = -1 * value
+    for k in range(4):
+        nx = x + dx[k] # 행
+        ny = y + dy[k] # 열
+        
+        if nx < 0 or nx >= m or ny < 0 or ny >= n:
             continue
-        for k in range(4):
-            nr = r + dr[k]
-            nc = c + dc[k]
-            if nr < 0 or nr >= M or nc < 0 or nc >= N:
+        
+        if value > board[nx][ny]:
+            dp[nx][ny] += dp[x][y]
+            if visited[nx][ny]:
                 continue
-            if board[nr][nc] > current:
-                s += dp[nr][nc]
-        dp[r][c] = s
-                
-print(dp[M-1][N-1])
+            visited[nx][ny] = True
+            heapq.heappush(queue, [-1 * board[nx][ny], nx, ny])
+            
+print(dp[m-1][n-1])
