406bf35a0873739868bebe3086c7ed148ff70875
chiyeon01 <cyj588369@gmail.com>
2026-01-03 14:06:35 +0900

    LV4
    Refactor variable type for 'start' and improve comments.

--- cpp/LEVEL4/BOJ_4375.cpp
+++ cpp/LEVEL4/BOJ_4375.cpp
@@ -3,12 +3,14 @@
 using namespace std;
 
 void solution(int n) {
-    long long int start = 1;
+    long long start = 1;
     int answer = 1;
 
     // 나누어 떨어질 때까지
     while (start % n != 0) {
+        // start는 prev_start를 모듈러 연산한 것에 기반
         start = (start % n) * 10 + 1;
+        // 현재 자리수 계산
         answer ++;
     }
 
