2e42910283839d7c8b436e6000b9a0173ab8934a
chiyeon01 <cyj588369@gmail.com>
2026-01-06 04:43:36 +0000

    LV6

---
+++ cpp/LEVEL6/BOJ_17425.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void solution(int N, vector<long>& f, vector<long>& g) {
+    int cnt = 0;
+    for (int i = 1; i < N + 1; i++) {
+        for (int j = i; j < N + 1; j+=i) {
+            f[j] += i;
+        }
+        g[i] = g[i-1] + f[i];
+    }
+}
+
+int main() {
+    vector<long> f(1000001);
+    vector<long> g(1000001);
+
+    solution(1000001, f, g);
+
+    int T, N;
+    cin >> T;
+    for (int i = 0; i < T; i++) {
+        scanf("%d", &N);
+        printf("%ld\n", g[N]);
+    }
+}
